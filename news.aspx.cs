47ecfa938d1f4c240c0657a92d2a4438748ced27
netnk <netnk0000@gmail.com>
2018-04-27 09:04:37 +0800

    Create news.aspx.cs

---
+++ news.aspx.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using web1;
+
+public partial class _Default : System.Web.UI.Page
+{
+    web1.website2.sqlhp sl = new web1.website2.sqlhp();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var dt = sl.query("select * from table");
+        repeater1.DataSource =dt;
+        repeater1.DataBind();
+    }
+}
